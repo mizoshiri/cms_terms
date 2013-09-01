2c09e44233394012c2ac83c3780d8781288a8a6c
Ayumi Mizoshiri <ayumi@sazae.com.au>
2013-09-01 10:29:13 +1000

    Add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CmsTerms::Application.config.secret_key_base = 'f2a17b7416d61c8f3412d715ea41e6b850dcbdd872b9873d775dbfe3eb087f711c3b3376c693f9aeae7d050d136debbecb380963e6bcfe87af82cc25e88bfdc2'
