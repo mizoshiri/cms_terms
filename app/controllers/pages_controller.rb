bb2a1225bf056e04460f523d1edac885c35a6b2a
Ayumi Mizoshiri <ayumi@sazae.com.au>
2013-09-01 14:43:56 +1000

    Add CanCane to Page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   before_filter :authenticate_user!
   before_action :set_page, only: [:show, :edit, :update, :destroy]
+  authorize_resource
 
   # GET /pages
   # GET /pages.json
